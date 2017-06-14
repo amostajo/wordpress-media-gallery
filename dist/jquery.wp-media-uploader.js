e800c44e0cba555d1cd1de96502cd3187817f9f7
Alejandro Mostajo <amostajo@gmail.com>
2017-06-14 12:51:14 -0600

    Adds release files.
    New distribution files.

--- dist/jquery.wp-media-uploader.js
+++ dist/jquery.wp-media-uploader.js
@@ -4,141 +4,232 @@
  * https://github.com/amostajo/wordpress-media-uploader
  *
  * @author Alejandro Mostajo
+ * @lincense MIT
+ * @version 1.0.4
  *
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/MIT
  */
 if (typeof jQuery === 'undefined') {
-	throw new Error('Wordpress Media Gallery requires jQuery');
+    throw new Error('Wordpress Media Gallery requires jQuery');
 }
 (function ($) {
 
-	'use strict';
+    'use strict';
 
-	// Create function
-	$.fn.mediaUploader = function (options)
-	{
-		// Plugin reference
-		var self = this;
+    /**
+     * Creates plugin.
+     * @since 1.0.0
+     */
+    $.fn.mediaUploader = function (options)
+    {
+        /**
+         * Plugin reference.
+         * @since 1.0.0
+         */
+        var self = this;
 
-		// Plugin options
-		self.settings = $.extend({
-			editor: undefined,
-			render: true,
-			clearTemplate: true,
-			clearTarget: true,
-			target: undefined,
-			success: undefined,
-			template: undefined
-		}, options );
+        /**
+         * Plugin options.
+         * @since 1.0.0
+         */
+        self.settings = $.extend({
+            editor: undefined,
+            render: true,
+            clearTemplate: true,
+            clearTarget: true,
+            target: undefined,
+            success: undefined,
+            template: undefined
+        }, options );
 
-		// Gets template from object
-		self._getTemplate = function ()
-		{
-			if (self.settings.template != undefined) return;
-			self.settings.template = $(self).html();
-			// Hide content
-			if (self.settings.clearTemplate)
-				$(self).html('');
-		}
+        /**
+         * Gets and sets template in which media uploaded will be displayed.
+         * @since 1.0.0
+         */
+        self._getTemplate = function ()
+        {
+            if (self.settings.template != undefined) return;
+            self.settings.template = $(self).html();
+            // Hide content
+            if (self.settings.clearTemplate)
+                $(self).html('');
+        }
 
-		// Renders media into target
-		self._render = function (media)
-		{
-			if (!self.settings.render) return;
-			if (self.settings.target != undefined 
-				&& $(self.settings.target).length > 0
-			) {
-				if (self.settings.clearTarget) $(self.settings.target).html('');
-				$.each(media, function (i) {
-					$(self.settings.target).append(
-						self._parseMedia(this)
-					);
-				});
-			}
-		}
+        /**
+         * Renders media into target.
+         * @since 1.0.0
+         */
+        self._render = function (media)
+        {
+            if (!self.settings.render) return;
+            if (self.settings.target != undefined 
+                && $(self.settings.target).length > 0
+            ) {
+                if (self.settings.clearTarget) $(self.settings.target).html('');
+                $.each(media, function (i) {
+                    $(self.settings.target).append(
+                        self._parseMedia(this)
+                    );
+                });
+            }
+        }
 
-		// Parses an individual media item and returns it as template
-		self._parseMedia = function (media) {
-			if (self.settings.template == undefined) return '';
-			var html = self.settings.template;
-			$.each(media, function (key) {
-				html = html.replace(new RegExp('{{' + key + '}}', 'g'), media[key], html);
-				html = html.replace(new RegExp('{{ ' + key + ' }}', 'g'), media[key], html);
-			});
-			html = $(html);
-			if (media.type == 'file' && $(html).find('img').length > 0) {
-				$(html).find('img').remove();
-			}
-			if (media.type == 'image' && $(html).find('img').length > 0) {
-				$(html).find('img').attr('src', media.url);
-			}
-			return html;
-		}
+        /**
+         * Parses an individual media item and returns it as template.
+         * @since 1.0.0
+         *
+         * @param object media Object with media details.
+         *
+         * @return string html
+         */
+        self._parseMedia = function (media) {
+            if (self.settings.template == undefined) return '';
+            var html = self.settings.template;
+            $.each(media, function (key) {
+                html = html.replace(new RegExp('{{' + key + '}}', 'g'), media[key], html);
+                html = html.replace(new RegExp('{{ ' + key + ' }}', 'g'), media[key], html);
+            });
+            html = $(html);
+            if (media.type == 'file' && $(html).find('img').length > 0) {
+                $(html).find('img').remove();
+            }
+            if (media.type == 'image' && $(html).find('img').length > 0) {
+                $(html).find('img').attr('src', media.url);
+            }
+            return html;
+        }
 
-		// Editor callback processing function
-		self._parseEditorHTML = function (html)
-		{
-			var media = [];
+        /**
+         * Parse HTML sent by wordpress editor.
+         * Editor callback processing function.
+         * @since 1.0.0
+         *
+         * @param string html HTML sent by wordpress editor.
+         */
+        self._parseEditorHTML = function (html)
+        {
+            var media = [];
 
-			$.each($(html), function (index) {
+            $.each($(html), function (index) {
 
-				if ($(this).find('img').length > 0) {
+                if ($(this).find('img').length > 0) {
 
-					media.push({
-						type: 'image',
-						url: $(this).find('img').attr('src'),
-						alt: $(this).find('img').attr('alt'),
-						id: $(this).find('img').attr('class').replace(/[A-Za-z\-\s]/g, '')
-					});
+                    media.push({
+                        type: 'image',
+                        url: $(this).find('img').attr('src'),
+                        alt: $(this).find('img').attr('alt'),
+                        id: $(this).find('img').attr('class').replace(/[A-Za-z\-\s]/g, '')
+                    });
 
-				} else if ($(this).is('img')) {
+                } else if ($(this).is('img')) {
 
-					media.push({
-						type: 'image',
-						url: $(this).attr('src'),
-						alt: $(this).attr('alt'),
-						id: $(this).attr('class').replace(/[A-Za-z\-\s]/g, '')
-					});
+                    media.push({
+                        type: 'image',
+                        url: $(this).attr('src'),
+                        alt: $(this).attr('alt'),
+                        id: $(this).attr('class').replace(/[A-Za-z\-\s]/g, '')
+                    });
 
-				} else if ($(this).attr('href') != undefined) {
+                } else if ($(this).attr('href') != undefined) {
 
-					media.push({
-						type: 'file',
-						url: $(this).attr('href')
-					});
-				}
+                    media.push({
+                        type: 'file',
+                        url: $(this).attr('href')
+                    });
+                }
 
-			});
+            });
 
-			self._render(media);
+            self._render(media);
 
-			if (self.settings.success != undefined) {
-				self.settings.success(media);
-			}
-		};
+            if (self.settings.success != undefined) {
+                self.settings.success(media);
+            }
+        };
 
-		// Save previous event
-		self.prev_send_to_editor = window.send_to_editor;
+        /**
+         * Saves previous event.
+         * @since 1.0.2
+         * @var object
+         */
+        self.prev_send_to_editor = window.send_to_editor;
 
-		// Editor callback
-		window.send_to_editor = function (html, editor)
-		{
-			if (self.settings.editor == undefined
-				|| (this.activeEditor != self.settings.editor
-				&& editor != self.settings.editor)
-			)
-				return self.prev_send_to_editor == undefined
-					? undefined
-					: self.prev_send_to_editor(
-						html,
-						this.activeEditor == undefined ? editor : this.activeEditor
-					);
+        /**
+         * Wordpress editor callback.
+         * @since 1.0.0
+         * @since 1.0.2 Fixes multiple callers.
+         *
+         * @param string html   HTML returned by wordpress editor.
+         * @param string editor Editor key name.
+         */
+        window.send_to_editor = function (html, editor)
+        {
+            if (self.settings.editor == undefined
+                || (this.activeEditor != self.settings.editor
+                && editor != self.settings.editor)
+            )
+                return self.prev_send_to_editor == undefined
+                    ? undefined
+                    : self.prev_send_to_editor(
+                        html,
+                        this.activeEditor == undefined ? editor : this.activeEditor
+                    );
 
-			self._parseEditorHTML(html);
-		};
+            self._parseEditorHTML(html);
+        };
 
-		// Get template
-		self._getTemplate();
-	};
+        /**
+         * Gets template.
+         * @since 1.0.0 
+         */
+        self._getTemplate();
+
+        /**
+         * Adds media to plugin.
+         * Thought to accessed as public method.
+         * @since 1.0.4
+         *
+         * @param mixed mediaToAdd
+         */
+        self._add_media = function(mediaToAdd)
+        {
+            var media = [];
+
+            // Convert param to array
+            if (mediaToAdd.constructor !== Array) {
+                var item = mediaToAdd;
+                mediaToAdd = [];
+                mediaToAdd.push(item);
+            }
+            for (var i in mediaToAdd) {
+                var item = {};
+                if (mediaToAdd[i].type === undefined
+                    && mediaToAdd[i].url === undefined
+                )
+                    continue;
+                item.type = mediaToAdd[i].type;
+                item.url = mediaToAdd[i].url;
+                if (mediaToAdd[i].alt !== undefined)
+                    item.alt = mediaToAdd[i].alt;
+                if (mediaToAdd[i].id !== undefined)
+                    item.id = mediaToAdd[i].id;
+                media.push(item);
+            }
+
+            self._render(media);
+
+            if (self.settings.success != undefined) {
+                self.settings.success(media);
+            }
+        }
+
+        /**
+         * Public method alias for add media function.
+         * @since 1.0.4
+         */
+        this.add = self._add_media;
+
+        return this;
+    };
 }(jQuery));
